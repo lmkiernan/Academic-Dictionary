91885bc330e8f7ac382ce92cd6e8c4327e111128
lmkiernan <liamkiernan03@gmail.com>
2025-04-30 10:43:31 -0400

    pops up manually upon clicking "define"

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// background.js
+
+// 1) On install, register the “Define” menu item
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "defineSelection",
@@ -6,9 +9,13 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
   
-  // When the user clicks that menu item, save the selection:
-  chrome.contextMenus.onClicked.addListener((info) => {
+  // 2) When the user clicks “Define …”:
+  chrome.contextMenus.onClicked.addListener(async (info) => {
     if (info.menuItemId === "defineSelection" && info.selectionText) {
-      chrome.storage.local.set({ lastSelection: info.selectionText });
+      // store the selected text so popup.js can read it
+      await chrome.storage.local.set({ lastSelection: info.selectionText });
+  
+      // open the extension’s popup immediately
+      chrome.action.openPopup();
     }
   });
