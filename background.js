0d69ee9faf576785b1a8fe16978268041b0507ae
lmkiernan <liamkiernan03@gmail.com>
2025-05-03 13:48:43 -0400

    added gemini capabilities attempt 1

--- background.js
+++ background.js
@@ -2,20 +2,16 @@
 
 // 1) On install, register the “Define” menu item
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: "defineSelection",
-      title: "Define \"%s\"",
-      contexts: ["selection"]
-    });
+  chrome.contextMenus.create({
+    id: "defineSelection",
+    title: "Define \"%s\"",
+    contexts: ["selection"]
   });
-  
-  // 2) When the user clicks “Define …”:
-  chrome.contextMenus.onClicked.addListener(async (info) => {
-    if (info.menuItemId === "defineSelection" && info.selectionText) {
-      // store the selected text so popup.js can read it
-      await chrome.storage.local.set({ lastSelection: info.selectionText });
-  
-      // open the extension’s popup immediately
-      chrome.action.openPopup();
-    }
-  });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info) => {
+  if (info.menuItemId === "defineSelection" && info.selectionText) {
+    await chrome.storage.local.set({ lastSelection: info.selectionText });
+    chrome.action.openPopup();
+  }
+});
